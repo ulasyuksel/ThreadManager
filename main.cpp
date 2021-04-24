61f456db48b0b8b3d542b262ecbd3840c9d537a4
Ulas Yuksel <ulasyuksel.tr@gmail.com>
2021-04-24 22:27:33 +0300

    argument handling and help edit

--- main.cpp
+++ main.cpp
@@ -31,21 +31,24 @@ static void usage(std::string name)
     std::cerr << "Usage: " << name << " <option(s)>"
               << "Options:\n"
               << "\t-h,--help\t\tShow this help message\n"
-              << "\t-N ,"
+              << "\t-N <# of thread>, #number of threads to be created greater than zero"
               << std::endl;
 }
 
 
 int main(int argc, char **argv)
 {
-    int return_val = -1;
+    int return_val = 1;
     if (argc == 2)
     {
         const std::string arg1(argv[1]);
         if (arg1 == "-h" || arg1 == "--help")
         {
             usage(argv[0]);
+            return_val = 0;
         }
+
+
     }
     else if (argc == 3)
     {
@@ -57,86 +60,84 @@ int main(int argc, char **argv)
             unsigned int number_of_threads;
             if (!(convert >> number_of_threads))
             {
-                usage(argv[0]);
                 return_val = 1;
             }
-            std::cout << "Creating " << number_of_threads << " threads\n";
-            std::string in_command;
-            ThreadManager my_local_thread_manager(number_of_threads);
-
-            while (true)
+            else if (number_of_threads > 0) // at least 1 thread should be created
             {
-                std::cout << RENDER_COMMAND_LIST;
-                getline (std::cin,in_command);
+                return_val = 0;
+                std::cout << "Creating " << number_of_threads << " threads\n";
+                std::string in_command;
+                ThreadManager my_local_thread_manager(number_of_threads);
 
-                if(in_command.find(QUIT_KEY) == 0)
+                while (true)
                 {
-                    std::cout << "quit" << std::endl;
-                    break;
-                }
-                else
-                {
-                    unsigned int thread_id;
-                    std::size_t pos = in_command.find(" ");
-                    if (pos != std::string::npos)
+                    std::cout << RENDER_COMMAND_LIST;
+                    getline (std::cin,in_command);
+
+                    if(in_command.find(QUIT_KEY) == 0)
                     {
-                        std::string id_str = in_command.substr (pos);
-                        std::stringstream string_convert{ id_str };
-                        if (!(string_convert >> thread_id))
-                        {
-                            std::cout << "wrong command format" << std::endl;
-                        }
-                        else
-                        {
-                            // Check the command which has to be run.
-                            if (in_command.find(STATUS_KEY) == 0)
-                            {
-                                std::cout << STATUS_KEY << ": " << thread_id << " --> " << threadStateToString(my_local_thread_manager.status(thread_id)) << std::endl;
-                            }
-                            else if(in_command.find(START_KEY) == 0)
-                            {
-                                std::cout << START_KEY << ": " << thread_id << std::endl;
-                                my_local_thread_manager.start(thread_id);
-                            }
-                            else if(in_command.find(ABORT_KEY) == 0)
-                            {
-                                std::cout << ABORT_KEY << ": " << thread_id << std::endl;
-                                my_local_thread_manager.abort(thread_id);
-                            }
-                            else if(in_command.find(PAUSE_KEY) == 0)
-                            {
-                                std::cout << PAUSE_KEY << ": " << thread_id << std::endl;
-                                my_local_thread_manager.pause(thread_id);
-                            }
-                            else if(in_command.find(RESUME_KEY) == 0)
-                            {
-                                std::cout << RESUME_KEY << ": " << thread_id << std::endl;
-                                my_local_thread_manager.resume(thread_id);
-                            }
-                            else
-                            {
-                                std::cout << "wrong command format" << std::endl;
-                            }
-                        }
+                        std::cout << "quit" << std::endl;
+                        break;
                     }
                     else
                     {
-                        std::cout << "wrong command format" << std::endl;
+                        unsigned int thread_id;
+                        std::size_t pos = in_command.find(" ");
+                        if (pos != std::string::npos)
+                        {
+                            std::string id_str = in_command.substr (pos);
+                            std::stringstream string_convert{ id_str };
+                            if (!(string_convert >> thread_id))
+                            {
+                                std::cout << "wrong command format" << std::endl;
+                            }
+                            else
+                            {
+                                // Check the command which has to be run.
+                                if (in_command.find(STATUS_KEY) == 0)
+                                {
+                                    std::cout << STATUS_KEY << ": " << thread_id << " --> " << threadStateToString(my_local_thread_manager.status(thread_id)) << std::endl;
+                                }
+                                else if(in_command.find(START_KEY) == 0)
+                                {
+                                    std::cout << START_KEY << ": " << thread_id << std::endl;
+                                    my_local_thread_manager.start(thread_id);
+                                }
+                                else if(in_command.find(ABORT_KEY) == 0)
+                                {
+                                    std::cout << ABORT_KEY << ": " << thread_id << std::endl;
+                                    my_local_thread_manager.abort(thread_id);
+                                }
+                                else if(in_command.find(PAUSE_KEY) == 0)
+                                {
+                                    std::cout << PAUSE_KEY << ": " << thread_id << std::endl;
+                                    my_local_thread_manager.pause(thread_id);
+                                }
+                                else if(in_command.find(RESUME_KEY) == 0)
+                                {
+                                    std::cout << RESUME_KEY << ": " << thread_id << std::endl;
+                                    my_local_thread_manager.resume(thread_id);
+                                }
+                                else
+                                {
+                                    std::cout << "wrong command format" << std::endl;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            std::cout << "wrong command format" << std::endl;
+                        }
                     }
-
                 }
             }
         }
-        else
-        {
-            usage(argv[0]);
-            return_val = 1;
-        }
+
     }
-    else
+
+    if (return_val != 0)
     {
         usage(argv[0]);
-        return_val = 1;
     }
     return return_val;
 }
